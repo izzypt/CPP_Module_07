1b127ee28cfecfb502044536de895284d055c94c
izzypt <bonvalots@gmail.com>
2023-12-11 17:59:21 +0000

    finished ex01

--- ex01/iter.hpp
+++ ex01/iter.hpp
@@ -1,12 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   iter.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: simao <simao@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/11 17:48:55 by simao             #+#    #+#             */
+/*   Updated: 2023/12/11 17:57:06 by simao            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef ITER_HPP
 #define ITER_HPP
 
 #include <iostream>
 
 template <typename T>
-void	iter(T *arr, T length, void (*f)(T const &))
+void	iter(T *arr, size_t length, void (*f)(T&))
 {
-    for (int i = 0; i < len; i++)
+    for (int i = 0; i < length; i++)
         f(arr[i]);
 }
 
--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,7 +6,30 @@
 /*   By: simao <simao@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/11 17:39:21 by simao             #+#    #+#             */
-/*   Updated: 2023/12/11 17:39:22 by simao            ###   ########.fr       */
+/*   Updated: 2023/12/11 17:58:40 by simao            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "iter.hpp"
+
+template <typename T>
+void print(T &x)
+{
+    std::cout << x << std::endl;
+    return;
+}
+
+int main(void)
+{
+    std::cout << "\n********** TEST 1 **********" << std::endl;
+    int tab[] = {0, 1, 2, 3, 4};
+
+    iter(tab, 5, print<int>);
+
+    std::cout << "\n********** TEST 2 **********" << std::endl;
+    std::string str[] = {"Hello", "World", "!"};
+
+    iter(str, 3, print<std::string>);
+
+    return (0);
+}
